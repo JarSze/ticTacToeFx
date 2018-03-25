f66aee2381addb869a6cac863472e1a6fe940429
jaroslav szeligowski <j.szeligowski@gmail.com>
2018-03-25 15:03:34 +0200

    next commit

--- src/com/javaLearning/MainWindowController.java
+++ src/com/javaLearning/MainWindowController.java
@@ -1,5 +1,12 @@
 package com.javaLearning;
 
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 public class MainWindowController
 {
+    public void handleClick(MouseEvent mouseEvent){
+        Label source = (Label)mouseEvent.getSource();
+        System.out.println(source.getId());
+    }
 }
